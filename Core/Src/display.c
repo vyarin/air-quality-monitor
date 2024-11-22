9064c2495bec6641050fad940fa5aa60130d4855
Vyarin <tranvyvivian@gmail.com>
2024-11-22 02:10:28 -0500

    Implement display_concentration

--- Core/Src/display.c
+++ Core/Src/display.c
@@ -1,5 +1,43 @@
 #include "display.h"
 
+// Display gas concentration; try three times
+display_status display_concentration(display_state const state) {
+    uint8_t tries = 3;
+
+    while (tries > 0) {    
+        switch (state) {
+        case 0:
+            if (display_PM25 == DISPLAY_FAIL) {
+                --tries;
+            } else {
+                return DISPLAY_OK;
+            }
+            break;
+        case 1:
+            if (display_O3 == DISPLAY_FAIL) {
+                --tries;
+            } else {
+                return DISPLAY_OK;
+            }
+            break;
+
+        case 2:
+            if (display_NO2 == DISPLAY_FAIL) {
+                --tries;
+            } else {
+                return DISPLAY_OK;
+            }
+            break;
+
+        default:
+            --tries;
+            break;
+        }
+    }
+
+    return DISPLAY_FAIL;
+}
+
 void cycle_status(display_state *state) {
     if (*state != 2) {
         state += 1;

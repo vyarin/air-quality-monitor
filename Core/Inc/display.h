2db6ce04b71669e25498430d8cc4e8a7878542f7
Vyarin <tranvyvivian@gmail.com>
2024-11-22 01:56:55 -0500

    Add display.h function declarations

---
+++ Core/Inc/display.h
@@ -0,0 +1,24 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+#include "sensor_data.h"
+
+typedef enum {
+	DISPLAY_OK = 1,
+	DISPLAY_FAIL = 0
+}
+display_status;
+
+typedef enum {
+	DISPLAY_PM25 = 0,
+	DISPLAY_O3 = 1,
+	DISPLAY_NO2 = 2
+}
+display_state;
+
+display_status display_PM25(sensor_data const *data);
+display_status display_O3(sensor_data const *data);
+display_status display_NO2(sensor_data const *data);
+
+display_status display_concentration(display_state const state);
+
+#endif

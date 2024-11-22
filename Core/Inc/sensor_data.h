c031d41a56a8ed8191777741428078a057e958b2
Vyarin <tranvyvivian@gmail.com>
2024-11-22 03:25:59 -0500

    Implement get_concentration

--- Core/Inc/sensor_data.h
+++ Core/Inc/sensor_data.h
@@ -12,4 +12,14 @@ typedef struct sensor_data {
 }
 sensor_data;
 
+uint16_t get_concentration(sensor_data const *data, uint8_t particle) {
+	if (particle == 0) {
+		return ((*data).PM25_high << 8) | ((*data).PM25_low);
+	} else if (particle == 1) {
+		return ((*data).O3_high << 8) | ((*data).O3_low);
+	} else {
+		return ((*data).NO2_high << 8) | ((*data).NO2_low);
+	}
+}
+
 #endif

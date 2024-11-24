c7b00af1b2efd4aec749f96d1b7f3999bde19aac
Alexia Rodriguez <a5morale@uwaterloo.ca>
2024-11-23 19:38:45 -0500

    Update MQ131.c

--- Core/Src/MQ131.c
+++ Core/Src/MQ131.c
@@ -11,20 +11,20 @@ uint16_t read_ADC(void){
 float voltage_to_ppb(float voltage){
 	if (voltage >= 4.0 || voltage < 1.7) {
 	        return 0.0f;
-	    }
+	}
 	else if (voltage < 4.0 && voltage >= 3.5) { // b/t 4 and 3.5 volts
 	        return (4.5f - voltage) * 75.0f;
-	    }
+	}
 	else if (voltage < 3.5 && voltage >= 3.0) { // b/t 3.5 and 3 volts
 	        return (4.0f - voltage) * 100.0f;
-	    }
+	}
 	else if (voltage < 3.0 && voltage >= 2.5) { // b/t 3.0 and 2.5 volts
-		        return (3.5f - voltage) * 200.0f;
-		    }
+		return (3.5f - voltage) * 200.0f;
+	}
 	else if (voltage < 2.5 && voltage >= 2.0) { // b/t 2.5 and 2.0 volts
-			        return (3.0f - voltage) * 400.0f;
-			    }
+		return (3.0f - voltage) * 400.0f;
+	}
 	else if (voltage < 2.0 && voltage >= 1.7) { // b/t 2.0 and 1.7 volts
-			        return (2.7f - voltage) * 1000.0f;
-			    }
+		return (2.7f - voltage) * 1000.0f;
+	}
 }

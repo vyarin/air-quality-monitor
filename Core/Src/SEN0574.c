c39e5a86775bc432c40cbf099910671762d8542a
Alexia Rodriguez <a5morale@uwaterloo.ca>
2024-11-23 19:09:31 -0500

    Update SEN0574.c with ADC reading function

--- Core/Src/SEN0574.c
+++ Core/Src/SEN0574.c
@@ -1,1 +1,10 @@
 #include "SEN0574.h"
+uint16_t read_ADC(void){
+  HAL_ADC_Start(&hadc1); //ADC conversion begins
+	if (HAL_ADC_PollForConversion(&hadc1, 100) == HAL_OK) {
+	        return HAL_ADC_GetValue(&hadc1); //reading ADC value
+	    }
+	    return 0;  // If something went wrong
+	}
+float voltage_to_PPB(float voltage){
+}

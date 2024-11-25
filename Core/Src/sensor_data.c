9f85d945769e8e230fa9eab6bd6db4beced098a6
Vyarin <tranvyvivian@gmail.com>
2024-11-24 22:05:15 -0500

    Remove const from get_concentration

--- Core/Src/sensor_data.c
+++ Core/Src/sensor_data.c
@@ -1,6 +1,6 @@
 #include "sensor_data.h"
 
-void get_concentration(sensor_data const *raw_data, gas_concentrations *data) {
+void get_concentration(sensor_data *raw_data, gas_concentrations *data) {
 	data->PM25 = ((*raw_data).PM25_high << 8) | ((*raw_data).PM25_low);
 	data->O3 = ((*raw_data).O3_high << 8) | ((*raw_data).O3_low);
 	data->NO2 = ((*raw_data).NO2_high << 8) | ((*raw_data).NO2_low);

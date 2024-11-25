da5eb9d92c20b1d8d7af462c6158f08f8c1c697f
Alexia Rodriguez <a5morale@uwaterloo.ca>
2024-11-25 01:56:03 -0500

    Initial Commit

---
+++ Core/Inc/LEDs.h
@@ -0,0 +1,5 @@
+#ifndef LEDs_H
+#define LEDs_H
+#include "main.h"
+void control_LEDs(float index);
+#endif
---
+++ Core/Inc/main.h
@@ -0,0 +1,90 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+
+#define LED1_PIN GPIO_PIN_0 // PA0
+#define LED2_PIN GPIO_PIN_1 // PA1
+#define LED3_PIN GPIO_PIN_0 // PC0
+#define LED4_PIN GPIO_PIN_4 // PA4
+#define LED5_PIN GPIO_PIN_1 // PC1
+#define LED6_PIN GPIO_PIN_0 // PB0
+#define LED7_PIN GPIO_PIN_5 // PB5
+#define LED8_PIN GPIO_PIN_3 // PB3
+
+// Define GPIO ports for LEDs
+#define LED1_PORT GPIOA
+#define LED2_PORT GPIOA
+#define LED3_PORT GPIOC
+#define LED4_PORT GPIOA
+#define LED5_PORT GPIOC
+#define LED6_PORT GPIOB
+#define LED7_PORT GPIOB
+#define LED8_PORT GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */

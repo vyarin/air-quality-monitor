622c27240fd0ec0447959efc8361f8859359702d
Vyarin <tranvyvivian@gmail.com>
2024-11-24 22:05:01 -0500

    Remove const keyword

--- Core/Inc/sensor_data.h
+++ Core/Inc/sensor_data.h
@@ -19,6 +19,6 @@ typedef struct gas_concentrations {
 }
 gas_concentrations;
 
-void get_concentration(sensor_data const *raw_data, gas_concentrations *data);
+void get_concentration(sensor_data *raw_data, gas_concentrations *data);
 
 #endif
